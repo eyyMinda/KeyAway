ff91383860e0af604c516ef990732158df151ac5
Mindaugas Straksys <gamerx14x@gmail.com>
2025-09-08 00:29:45 +0300

    expanded: tailwind config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,28 @@
 import type { NextConfig } from "next";
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+
+const imageProviders: RemotePattern[] = [
+  {
+    protocol: "https",
+    hostname: "cdn.sanity.io",
+    pathname: "/**"
+  },
+  {
+    protocol: "https",
+    hostname: "avatars.githubusercontent.com",
+    pathname: "/u/**"
+  },
+  {
+    protocol: "https",
+    hostname: "raw.githubusercontent.com",
+    pathname: "/**"
+  }
+];
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io"]
+    remotePatterns: imageProviders
   }
 };
 
